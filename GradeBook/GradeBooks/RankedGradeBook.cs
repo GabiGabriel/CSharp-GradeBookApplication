898aebb40e9fa7bf871d3ad870d3f128123524cb
GabiGabriel <ciocangabrieel@gmail.com>
2018-03-14 10:33:40 +0200

    Added new classes : RankedGradeBook and StandardGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public string Name { get; set; }
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.Name = name;
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public string Name { get; set; }
+        public GradeBookType Type { get; set; }
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Name = name;
+            Type = GradeBookType.Standard;
+        }
+    }
+}
