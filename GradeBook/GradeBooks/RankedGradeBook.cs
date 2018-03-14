be829111cca9b52669110b2eac437c3a3fdf3de1
GabiGabriel <ciocangabrieel@gmail.com>
2018-03-14 11:22:36 +0200

    Modified The GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,26 +14,15 @@ namespace GradeBook.GradeBooks
             this.Name = name;
             Type = GradeBookType.Ranked;
         }
-        public override char GetLetterGrade(double avarageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count <5)
-            {
-                Console.WriteLine("The number of students should be grater than 5");
+            if (Students.Count < 5)
                 throw new InvalidOperationException();
-            }
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            var grades = Students.OrderByDescending(j=>j.AverageGrade).Select(e=>e.AverageGrade).ToList();
 
-            if (grades[threshold - 1] <= avarageGrade)
-                return 'A';
-            else if (grades[(threshold * 2) - 1] <= avarageGrade)
-                return 'B';
-            else if (grades[(threshold * 3) - 1] <= avarageGrade)
-                return 'C';
-            else if (grades[(threshold * 4) - 1] <= avarageGrade)
-                return 'D';
 
+           
             return 'F';
         }
+
     }
 }
