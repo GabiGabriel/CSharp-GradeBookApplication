2fd0595e6153871e3232e21d7b350f777f703635
GabiGabriel <ciocangabrieel@gmail.com>
2018-03-14 10:36:19 +0200

    Made the two new added classes - public

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public string Name { get; set; }
         public RankedGradeBook(string name) : base(name)
