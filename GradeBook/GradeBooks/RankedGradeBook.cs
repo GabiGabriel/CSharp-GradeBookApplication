519561a43103de7e0971143438ecdb4fef95d943
GabiGabriel <ciocangabrieel@gmail.com>
2018-03-14 12:12:53 +0200

    Insterted an condition statement into GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,11 +21,15 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students[0].AverageGrade * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(x => x.AverageGrade).ToList();
 
-            if (grades[threshold] - 1 <= averageGrade)
+            if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            else if (grades[threshold * 2] - 1 <= averageGrade)
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
             return 'F';
         }
        
